89b0c5397cec48562b340a723a1a8f799a3d938e
AnthonyM <tonymiyo132@gmail.com>
2024-10-30 21:41:44 -0500

    Updated fragment_home and HomeFragment

--- app/src/main/java/com/example/loginsample/fragments/HomeFragment.java
+++ app/src/main/java/com/example/loginsample/fragments/HomeFragment.java
@@ -1,66 +1,53 @@
 package com.example.loginsample.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.loginsample.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Configura los botones o elementos que respondan a los clicks
+        Button edificacionesButton = view.findViewById(R.id.button_edificaciones);
+        Button mapaButton = view.findViewById(R.id.button_mapa);
+
+        // Asigna los métodos de clic
+        edificacionesButton.setOnClickListener(this::onEdificacionesClick);
+        mapaButton.setOnClickListener(this::onMapClick);
+
+        return view;
+    }
+
+    public void onEdificacionesClick(View view) {
+        ListaFragments edificacionesFragment = new ListaFragments();
+        requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, edificacionesFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void onMapClick(View view) {
+        MapaFragment mapaFragment = new MapaFragment();
+        requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, mapaFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
